8853e0d0b6eea03d431262f0d81d60d22670920c
Carolina Hernandez <hernandez.carolina@live.cl>
2023-05-03 21:49:36 -0400

    Cambios js

--- mov.js
+++ mov.js
@@ -8,12 +8,53 @@ const  raza = document.getElementById("raza")
 
 const agregar = document.getElementById("agregar")
 
+const  tabla = document.getElementById('tablabody')
+
+let arregloMascotas = []
+
+
 agregar.addEventListener("click", (event) => {
     event.preventDefault()
-const mascota = {
-    nombre:  nombre.value,
-    tipo: tipo.value,
-    raza: raza.value
-}
+const mascota = crearMascota()
 console.log(mascota)
 } )
+
+function crearMascota (){
+    const mascota = {
+        id: Date.now(),
+        nombre:  nombre.value,
+        tipo: tipo.value,
+        raza: raza.value
+    }
+    arregloMascotas.push(mascota)
+    localStorage.setItem('mascotas', JSON.stringify(arregloMascotas))
+    return mascota
+}      
+
+function  obtenerLS  (){
+    const data = localStorage.getItem('mascotas')
+    const dataManipulable = JSON.parse(data) 
+    if(data){
+        arregloMascotas = dataManipulable
+    } else {
+        arregloMascotas = []
+    }
+    tabla.innerHTML =  ''
+    arregloMascotas.forEach((mascota) => {
+        tabla.appendChild(`
+        <tr>
+        <td>${mascota.id}</td>
+        <td>${mascota.nombre}</td>
+        <td>${mascota.tipo}</td>
+        <td>${mascota.raza}</td>
+        <td><button id="editar">EDITAR</button>
+        <button id="eliminar">ELIMINAR</button>
+        </td>
+        </tr>
+        `)
+    })
+    
+}
+obtenerLS()
+
+
