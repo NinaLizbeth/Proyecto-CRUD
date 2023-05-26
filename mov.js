f464da1cea8ed5c864fcc0de080dd1cf745a79bd
Carolina Hernandez <hernandez.carolina@live.cl>
2023-05-26 15:37:37 -0400

    funcionalidad buscador

--- mov.js
+++ mov.js
@@ -13,6 +13,10 @@ const tabla = document.getElementById('tablabody')
 
 const actualizar = document.getElementById("actualizar")
 
+const buscador = document.getElementById('buscador')
+
+const searchB = document.getElementById('searchB')
+
 // utilizo array vacio para  luego ir agregando las mascotas
 let arregloMascotas = []
 //  se iguala a null porque en el estado inicial no se esta editando ninguna  mascota
@@ -36,8 +40,8 @@ function crearMascota() {
     // guardo el objeto mascota dentro de mi array de mascotas y le aplico el metodo push para agregarla al arreglo.
     arregloMascotas.push(mascota)
     // Llamo a la funcion guardarEnLS() para agregarla al localStorage.
-guardarEnLS()
-// con esta funcion se agregara una nueva fila con los datos del objeto mascota
+    guardarEnLS()
+    // con esta funcion se agregara una nueva fila con los datos del objeto mascota
     crearFila(mascota)
     // utilizo esta funcion para resetear el formulario una vez ingresado los datos
     borrarFormulario()
@@ -52,7 +56,7 @@ function obtenerLS() {
     if (data) {
         arregloMascotas = dataManipulable
         arregloMascotas.forEach((mascota) => {
-    crearFila(mascota)
+            crearFila(mascota)
         })
     } else {
         arregloMascotas = []
@@ -60,11 +64,9 @@ function obtenerLS() {
 
 
 }
-// Aqui se llama a la funcion obtenerLs
-obtenerLS()
 
 // Aqui se crea  la funcion crearFila mencionada arriba para agregara una nueva fila con los datos del objeto mascota  utilizo tabla.innerHTML para agregar una nueva fila a la constante tabla ("tablabody")
-function crearFila (mascota){
+function crearFila(mascota) {
     tabla.innerHTML += `
     <tr>
     <td>${mascota.id}</td>
@@ -79,68 +81,98 @@ function crearFila (mascota){
     `
 }
 // Aqui se crea la funcion borrar formulario que nos sirve para resetear el formulario cada vez que se ingresa una mascota.
-function borrarFormulario (){
+function borrarFormulario() {
     formulario.reset()
 }
 //  Aqui se crea la funcion BorrarFila para poder eliminar la fila correspondiente al id de la mascota que se entrega. EL método findIndex me encuentra el índice de la mascota que se va a eliminar.  (m) => m.id == id, busca la primera mascota del arregloMascotas que su id coincide con el id que se dio como argumento.
-function borrarFila (id){
-const indice = arregloMascotas.findIndex((m) =>m.id == id )
-// Aquí con el metodo splice borramos  una mascota del array de mascotas, tenemos dos argumentos a eliminar indice que es el lugar en el array donde esta el id de la mascota indicada y el 1 nos dice que borrara solo un elemento.
-arregloMascotas.splice(indice, 1)
-// Llamamos a la funcion guardarEnLS para actualizar la informacion en el LocalStorage.
-guardarEnLS()
-// Con tabla.innerHTML = '' Borramos  todas las filas para agregar las filas actualizadas
-tabla.innerHTML = ''
-//  Se llama a la función obtenerLS paracargar los datos de las mascotas guardados en el localStorage y actualizar la tabla.
-obtenerLS()
+function borrarFila(id) {
+    const indice = arregloMascotas.findIndex((m) => m.id == id)
+    // Aquí con el metodo splice borramos  una mascota del array de mascotas, tenemos dos argumentos a eliminar indice que es el lugar en el array donde esta el id de la mascota indicada y el 1 nos dice que borrara solo un elemento.
+    arregloMascotas.splice(indice, 1)
+    // Llamamos a la funcion guardarEnLS para actualizar la informacion en el LocalStorage.
+    guardarEnLS()
+    // Con tabla.innerHTML = '' Borramos  todas las filas para agregar las filas actualizadas
+    tabla.innerHTML = ''
+    //  Se llama a la función obtenerLS paracargar los datos de las mascotas guardados en el localStorage y actualizar la tabla.
+    obtenerLS()
 }
 // Llamamos a la funcion guardarEnLS para actualizar la informacion en el LocalStorage.
 // JSON.stringify convierte mi arreglo en texto plano
-function guardarEnLS (){
+function guardarEnLS() {
     localStorage.setItem('mascotas', JSON.stringify(arregloMascotas))
 }
 
 // funcion para editar la fila que recibe como parametro el id para buscar la mascota a editar
-function editarFila (id){
-// igualamos  el idmascotaEdit con el id de la mascota que estamos editando en ese momento.
+function editarFila(id) {
+    // igualamos  el idmascotaEdit con el id de la mascota que estamos editando en ese momento.
     idmascotaEdit = id
     // aqui ocupo el metodo find para buscar la mascota con el id del parametro y guardaela en la constante (ocupe la misma tecnica utilizada en borrar fila)
-const mascota = arregloMascotas.find((m) =>m.id == id )
-// Con actualizar llamo a mi boton y ocupo los metodos classList y remove para poder quitar  la clase (para quitar display none de css)
-actualizar.classList.remove('oculto')
-// Al boton agregar se le añade la clase oculto, para que tenga las propiedades de display none en  css
-agregar.classList.add('oculto')
+    const mascota = arregloMascotas.find((m) => m.id == id)
+    // Con actualizar llamo a mi boton y ocupo los metodos classList y remove para poder quitar  la clase (para quitar display none de css)
+    actualizar.classList.remove('oculto')
+    // Al boton agregar se le añade la clase oculto, para que tenga las propiedades de display none en  css
+    agregar.classList.add('oculto')
 
-//Ocupo la condicional if  para  ver si mascota tiene el mismo valor que el tipo? aqui no estoy segura
-if(mascota){
-    nombre.value = mascota.nombre;
-    tipo.value = mascota.tipo;
-    raza.value = mascota.raza;
+    //Ocupo la condicional if  para  ver si mascota tiene el mismo valor que el tipo? aqui no estoy segura
+    if (mascota) {
+        nombre.value = mascota.nombre;
+        tipo.value = mascota.tipo;
+        raza.value = mascota.raza;
 
-}
+    }
 }
 // Se crea la funcion que se llama cuando hacemos click en el boton de actualizar
-function editarMascota (event){
-event.preventDefault()
-// Creamos el objeto mascota con los datos del formulario una vez que este sea modificado.
-const mascota = {
-    id: idmascotaEdit,
-    nombre: nombre.value,
-    tipo: tipo.value,
-    raza: raza.value
-}
-// buscamos la posicion en el arreglo de la mascota que estamos editando
-const index  = arregloMascotas.findIndex(masc => masc.id == idmascotaEdit)
-// modifica la mascota encontrada en la posicion index con los nuevos datos del formulario
-arregloMascotas[index] = mascota
-guardarEnLS()
-borrarFormulario()
-actualizar.classList.add('oculto')
-agregar.classList.remove('oculto')
-idmascotaEdit = null
-tabla.innerHTML = ''
-obtenerLS()
+function editarMascota(event) {
+    event.preventDefault()
+    // Creamos el objeto mascota con los datos del formulario una vez que este sea modificado.
+    const mascota = {
+        id: idmascotaEdit,
+        nombre: nombre.value,
+        tipo: tipo.value,
+        raza: raza.value
+    }
+    // buscamos la posicion en el arreglo de la mascota que estamos editando
+    const index = arregloMascotas.findIndex(masc => masc.id == idmascotaEdit)
+    // modifica la mascota encontrada en la posicion index con los nuevos datos del formulario
+    arregloMascotas[index] = mascota
+    guardarEnLS()
+    borrarFormulario()
+    actualizar.classList.add('oculto')
+    agregar.classList.remove('oculto')
+    idmascotaEdit = null
+    tabla.innerHTML = ''
+    obtenerLS()
 
 }
 // Aqui escuchamos el click en el boton actualizar
 actualizar.addEventListener('click', editarMascota)
+
+// ----------------------------------------------------------------------
+// Agre un escuchador de evento para que cuando cambie el input  llama la funcion buscar mascota
+buscador.addEventListener('input', buscarMascota)
+// creo la función que me va a servir para buscar una mascota
+function buscarMascota() {
+  // obtengo el valor del buscador y con toLowerCase convertimos todo a minuscula  para que la busqueda no se enfoque en solo mayusculas y minusculas.
+  const textoBusqueda = buscador.value.toLowerCase();  
+  
+  // Limpio la tabla para que solo se muestren los resultados de lo que estoy buscando
+  tabla.innerHTML = "";
+
+// Creo constante de newArray que tendra todas mascotas que cumplan con la condicion del filter.
+    const newArray = arregloMascotas.filter((masc)=>masc.nombre.toLowerCase().includes(textoBusqueda))
+    // Si encuentra mas de un elemento lo va mapear en la tabla.
+    if (newArray.length >= 1) {
+      // si es asi, se crea una nueva fila con la mascota que se encontró
+      newArray.forEach((elemento)=>crearFila(elemento)) 
+    //   S no, nos retorna un mensaje de error.
+    } else{
+        tabla.innerHTML = `
+        <div>
+       <h3>No se encontraron resultados para su busqueda</h3> 
+        </div>
+        `
+    }
+  };
+
+obtenerLS()
+
